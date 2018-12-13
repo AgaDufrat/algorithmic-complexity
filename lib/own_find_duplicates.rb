a02575b31a5df17c5e496fd390732136f7e9d597
Aga <a.dufrat@gmail.com>
2018-12-13 14:30:20 +0000

    Add own find_duplicates and reverse methods

---
+++ lib/own_find_duplicates.rb
@@ -0,0 +1,20 @@
+
+def own_find_duplicates(array)
+  duplicates = []
+  for i in 0..array.size do
+    for j in (i + 1)..array.size do
+      if array[i] == array[j]
+        duplicates << array[i]
+      end
+    end
+  end
+  duplicates
+end
+
+def own_reverse(array)
+  new_array = []
+  array.size.times {
+    new_array << array.pop
+  }
+  new_array
+end
---
+++ spec/find_duplicates_spec.rb
@@ -0,0 +1,28 @@
+require 'own_find_duplicates'
+
+describe '#own_find_duplicates' do
+  it 'finds 1 duplicate' do
+    array = [1,2,3,4,5,1]
+    expect(own_find_duplicates(array)).to eq([1])
+  end
+  it 'finds 2 duplicates' do
+    array = [1,2,3,4,5,1,2]
+    expect(own_find_duplicates(array)).to eq([1,2])
+  end
+  it 'returns empty array if there is no duplicates' do
+    array = [1,2,3,4,5]
+    expect(own_find_duplicates(array)).to eq([])
+  end
+end
+
+describe '#own_reverse' do
+  it 'reverses short ordered array' do
+    array = [1,2,3]
+    expect(own_reverse(array)).to eq([3,2,1])
+  end
+
+  it 'reverses long array' do
+    array = [9,8,7,1,2,3]
+    expect(own_reverse(array)).to eq([3,2,1,7,8,9])
+  end
+end
